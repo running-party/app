6110465de095cade976539355cfdb63e97d36497
AHN,SEONG-JOON <84303387+aszzoon@users.noreply.github.com>
2024-10-28 16:11:34 +0900

    refactor: profil_dto

--- lib/dto/profile_dto.dart
+++ lib/dto/profile_dto.dart
@@ -4,13 +4,19 @@ class ProfileDto {
   final String email;
   final String profilePictureUrl;
 
-  ProfileDto({required this.name, required this.email, required this.profilePictureUrl});
+  ProfileDto({required this.name, required this.email, required this.profilePictureUrl, required String username, required String userId, required String profileImageUrl});
 
   factory ProfileDto.fromJson(Map<String, dynamic> json) {
     return ProfileDto(
       name: json['name'],
       email: json['email'],
-      profilePictureUrl: json['profilePictureUrl'],
+      profilePictureUrl: json['profilePictureUrl'], username: '', userId: '', profileImageUrl: '',
     );
   }
+
+  String? get username => null;
+
+  String? get profileImageUrl => null;
+
+  get userId => null;
 }
